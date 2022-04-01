816a4c93bdf5bd6ab716507f4ccff12e9d0bc8b4
varsha-504 <antargangivarsha.com>
2022-04-01 17:09:05 +0530

    added linked list program

---
+++ List1.java
@@ -0,0 +1,36 @@
+package com.bl.linkedlistprogram;
+
+import java.util.LinkedList;
+
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class List1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to LinkedList Datastructure Program");
+        LinkedList<Integer> List1 = new LinkedList<Integer>();
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+
+        }
+
+    }
+}
