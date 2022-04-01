e0a30578da61bff705074c524aefb9457ec3bb91
varsha-504 <antargangivarsha.com>
2022-04-02 00:08:15 +0530

    ability to delete last

--- LinkedList.java
+++ LinkedList.java
@@ -34,7 +34,7 @@ public class LinkedList {
     /*
      * This is the method to display elements
      */
-    public void display() {
+    public void display(Object data) {
         // list is empty
         if (head == null)
             System.out.println("No elements to display.");
@@ -87,6 +87,24 @@ public class LinkedList {
         else {
             Node temp = head;
             head = temp.ref;
+
+            public void deleteLast () {
+                // list is empty
+                if (head == null)
+                    System.out.println("No elements to delete");
+                    // list has only one element
+                else if (head.ref == null)
+                    head = null;
+                    // list is not empty
+                else {
+                    Node temp = head;
+                    // traverse up to ref of next node is null
+                    while (temp.ref.ref != null) {
+                        temp = temp.ref;
+                    }
+                    temp.ref = null;
+                }
+            }
         }
     }
 }
--- List1.java
+++ List1.java
@@ -5,13 +5,13 @@ import java.util.LinkedList;
 public class List1 {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList Datastructure Program");
-        LinkedList list = new LinkedList();
-        list.addFirst(70);
-        list.addFirst(30);
-        list.addFirst(56);
-        list.display();
-        list.deleteFirst();
-        list.display();
+        LinkedList list1 = new LinkedList();
+        list1.addFirst(70);
+        list1.addFirst(30);
+        list1.addFirst(56);
+        list1.display();
+        list1.deleteLast();
+        list1.display();
 
 
     }
